75de591307c8b68d40782b5972286ed485986208
Christian Guerin <cjguerin93@gmail.com>
2016-02-27 19:45:25 -0500

    more app.js, initial react commits

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var React = require('react');
+
+var alexaButton = require('./alexaButton');
+var textList = require('./textList');
+
 var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
@@ -52,3 +57,112 @@ app.post('/message', function (req, res) {
   var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
   });
+
+
+var App = React.createClass({
+        getInitialState(){
+
+        // Extract existing to-dos from local storage (DynamoDB)
+
+        var todos = [];
+
+        if (localStorage.todos){
+                todos = JSON.parse(localStorage.todos);
+        }
+
+        // return an example task if list is empty
+
+        return{
+          todos: todos,
+          scheduledCompletion: {
+            date: DateNow();
+            time: date.getTime();
+          }
+        };
+
+      },
+
+      toggleRecurring(todos){
+        if(this.isTodoInRecurring(todos)){
+          this.removeFromRecurring(todos);
+        }
+        else{
+          this.addToRecurring(todos);
+      }
+    },
+
+    addToRecurring(todos){
+
+      var recurring = this.state.recurring;
+      var d = new Date();
+
+      recurring.push({
+        day: d.getDay();
+        time: d.getTime();
+      });
+
+      this.setState({
+        recurring: recurring;
+      });
+
+      localStorage.recurring = JSON.stringify(favorites);
+    },
+
+    removeFromRecurring(todos){
+      var recurring = this.state.recurring;
+      var index = -1;
+
+      for(var i = 0; recurring.length; i++){
+
+        if(recurring[i].todos == todos){
+          index = i;
+          break;
+        }
+      }
+
+        // If it was found, remove task from the recurring array
+
+        if(index !== -1){
+
+          favorites.splice(index, 1);
+
+          this.setState({
+            recurring: recurring;
+          });
+
+          localStorage.recurring = JSON.stringify(recurring);
+        }
+      },
+
+      isTodoInRecurring(todos){
+
+        var recurring = this.state.recurring;
+
+        for(var i = 0; i < recurring.length ; i++){
+
+          if(recurring[i].todos == todos){
+            return true;
+          }
+        }
+        return false;
+      },
+
+      render(){
+
+        return (
+
+          <div>
+            <h1>Your Scheduled Text Messages</h1>
+
+            <textList todos={this.state.recurring} activeTodos={this.state.scheduledCompletion} />
+
+          </div>
+
+        );
+
+      };
+
+
+});
+
+module.exports = App;
